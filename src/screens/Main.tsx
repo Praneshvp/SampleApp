79efb249e94bd1aa8d626ce61dcdfccdceac6ae2
Pranesh V <pranesh@firesky.cc>
2022-03-13 16:33:04 +0530

    Updated navigation and api calls

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,34 +1,72 @@
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
- 
- const Main = () => {
-   const isDarkMode = useColorScheme() === 'dark';
- 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
- 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <Text style={{marginTop: 45}}>Main</Text>
-     </SafeAreaView>
-   );
- };
- 
- const styles = StyleSheet.create({
+import * as React from 'react';
+import { Image, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Colors from '../utils/Colors';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import ListItems from './ListItems'
+import Profile from './Profile'
 
- });
- 
- export default Main;
- 
+const Tab = createBottomTabNavigator();
+
+export default function TabRoutes() {
+
+  return (
+
+    <NavigationContainer>
+
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: Colors.THEME,
+          tabBarInactiveTintColor: Colors.BLACK,
+        }}
+      >
+        <Tab.Screen name="Home" component={ListItems}
+          options={({ route }) => ({
+            headerStyle:{
+              backgroundColor: Colors.THEME
+            },
+            headerTitleStyle:{
+              color: Colors.WHITE
+            },
+            tabBarIcon: ({ focused, color }) => (
+              focused ?
+                <Image source={require('../assets/home.png')} style={{
+                  width: hp('3%'), height: hp('3%'),
+                  tintColor: Colors.THEME
+                }} />
+                :
+                <Image source={require('../assets/home.png')} style={{ width: hp('3%'), height: hp('3%') }} />
+            ),
+          })} />
+
+
+
+        <Tab.Screen name="Profile" component={Profile}
+          options={({ route }) => ({
+            headerStyle:{
+              backgroundColor: Colors.THEME
+            },
+            headerTitleStyle:{
+              color: Colors.WHITE
+            },
+            tabBarIcon: ({ focused, color }) => (
+              focused ?
+                <Image source={require('../assets/profile.png')} style={{
+                  width: hp('3%'), height: hp('3%'),
+                  tintColor: Colors.THEME
+                }} />
+                :
+                <Image source={require('../assets/profile.png')} style={{ width: hp('3%'), height: hp('3%') }} />
+            ),
+          })} />
+
+
+
+      </Tab.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
